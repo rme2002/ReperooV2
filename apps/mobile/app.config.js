44b1eb4cae803df01dd6f4c994f512b3dcacc6c3
rme2002 <87778119+rme2002@users.noreply.github.com>
2026-01-25 23:05:50 +0100

    chore: changed mobile app config to reperoo

--- apps/mobile/app.config.js
+++ apps/mobile/app.config.js
@@ -5,19 +5,19 @@ export default ({ config }) => {
   const isProd = variant === "prd";
 
   // Base identifier from your original app.json
-  const baseId = "com.rjaay23.startermono";
+  const baseId = "com.reperoo.app";
 
   return {
     // start from Expo's defaults
     ...config,
 
     // ----- generic app settings -----
-    name: isProd ? "Starter Mono" : "Starter Mono dev",
-    slug: "starter-mono-mobile",
-    version: "0.0.9",
+    name: isProd ? "Reperoo" : "Reperoo Dev",
+    slug: "mobile",
+    version: "1.0.1",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
-    scheme: "startermono",
+    scheme: "mobile",
     userInterfaceStyle: "automatic",
     newArchEnabled: true,
 
@@ -67,11 +67,11 @@ export default ({ config }) => {
       ...(config.extra || {}),
       router: {},
       eas: {
-        projectId: "a03dd2a6-0203-4177-9330-880a4a87e3be" // replace with your own expo eas project id, set to undefined for first build so cli generates on for you
+        projectId: "a9fc8903-843b-4386-beb9-fdb62771973a"
       },
       appVariant: variant // handy to read inside the app if needed
     },
 
-    owner: "rjaay23"
+    owner: "rme2002"
   };
 };
